3fb1498935650bce4bc7176b1ac07050438ced69
Amar Panjwani <amar.panjwani@gmail.com>
2021-09-09 21:39:59 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const pattern = ["red","green","blue","black"];
+const pattern1 = ["red","green","blue","black"];
+const pattern2 = ["red","green","black","blue"];
+
+if(pattern[0] == pattern1[0]){
+    console.log("Pat and Pat 1 - 1 match")
+}else{
+    console.log("Pat and Pat1 don't match")
+}
+if(pattern == pattern2){
+    console.log("Pat and Pat2 match")
+}else{
+    console.log("Pat and Pat2 don't match")
+}
