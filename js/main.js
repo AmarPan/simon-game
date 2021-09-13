6f97ea567b96710ff828d554c4d90692018f6b8a
Amar Panjwani <amar.panjwani@gmail.com>
2021-09-13 12:08:45 -0700

    Game buttons done

--- js/main.js
+++ js/main.js
@@ -1,14 +1,53 @@
-const pattern = ["red","green","blue","black"];
-const pattern1 = ["red","green","blue","black"];
-const pattern2 = ["red","green","black","blue"];
+let compChoices = [];
+let playerChoices = [];
 
-if(pattern[0] == pattern1[0]){
-    console.log("Pat and Pat 1 - 1 match")
-}else{
-    console.log("Pat and Pat1 don't match")
+btnPink.addEventListener("click", function(evt){
+    playerChoices.push("pink");
+    document.getElementById("btnPink").style.backgroundColor = "hotpink";
+    setTimeout(function(){
+        document.getElementById("btnPink").style.backgroundColor = "black";
+    },200);
+    console.log("playerChoices:",playerChoices);
+});
+
+btnGreen.addEventListener("click", function(evt){
+    playerChoices.push("green");
+    document.getElementById("btnGreen").style.backgroundColor = "limegreen";
+    setTimeout(function(){
+        document.getElementById("btnGreen").style.backgroundColor = "black";
+    },200);
+    console.log("playerChoices:",playerChoices);
+});
+
+btnBlue.addEventListener("click", function(evt){
+    playerChoices.push("blue");
+    document.getElementById("btnBlue").style.backgroundColor = "aqua";
+    setTimeout(function(){
+        document.getElementById("btnBlue").style.backgroundColor = "black";
+    },200);
+    console.log("playerChoices:",playerChoices);
+});
+
+btnYellow.addEventListener("click", function(evt){
+    playerChoices.push("yellow");
+    document.getElementById("btnYellow").style.backgroundColor = "yellow";
+    setTimeout(function(){
+        document.getElementById("btnYellow").style.backgroundColor = "black";
+    },200);
+    console.log("playerChoices:",playerChoices);
+});
+
+function genRandColor(){
+    let colors = ["pink", "green", "blue", "yellow"];
+    let rand = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
+    console.log("Rand:",rand, colors[rand]);
+    return colors[rand];
 }
-if(pattern == pattern2){
-    console.log("Pat and Pat2 match")
-}else{
-    console.log("Pat and Pat2 don't match")
+
+function addCompChoice(){
+    console.log(compChoices)
+    compChoices.push(genRandColor());
+    console.log("compChoices:",compChoices)
 }
+
+addCompChoice();
