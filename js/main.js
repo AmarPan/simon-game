863675eaef236a1ef4496d9d846fb3e16c1f9684
Amar Panjwani <amar.panjwani@gmail.com>
2021-09-14 10:38:04 -0700

    Interval clicks working

--- js/main.js
+++ js/main.js
@@ -1,53 +1,168 @@
-let compChoices = [];
+let compChoices = ["pink","green"];
 let playerChoices = [];
+let playerTurn = false;
+let pushStatus = false;
 
-btnPink.addEventListener("click", function(evt){
-    playerChoices.push("pink");
+btnPink.addEventListener("click", clickPink);
+
+btnGreen.addEventListener("click", clickGreen);
+
+btnBlue.addEventListener("click", clickBlue);
+
+btnYellow.addEventListener("click", clickYellow);
+
+btnTurn.addEventListener("click", changeTurn)
+
+btnCheck.addEventListener("click",checkAnswer)
+
+function clickPink(){
     document.getElementById("btnPink").style.backgroundColor = "hotpink";
     setTimeout(function(){
         document.getElementById("btnPink").style.backgroundColor = "black";
-    },200);
-    console.log("playerChoices:",playerChoices);
-});
+    },300);
+    if(playerTurn && pushStatus){
+        playerChoices.push("pink");
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"playerChoices:",playerChoices)
+    }else if(playerTurn && !pushStatus){
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"playerChoices:",playerChoices)
+    }else if(!playerTurn && pushStatus){
+        compChoices.push("pink");
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"compChoices:",compChoices)
+    }else if(!playerTurn && !pushStatus){
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"compChoices:",compChoices)
+    }
+}
 
-btnGreen.addEventListener("click", function(evt){
-    playerChoices.push("green");
+function clickGreen(){
     document.getElementById("btnGreen").style.backgroundColor = "limegreen";
     setTimeout(function(){
         document.getElementById("btnGreen").style.backgroundColor = "black";
-    },200);
-    console.log("playerChoices:",playerChoices);
-});
+    },300);
+    if(playerTurn && pushStatus){
+        playerChoices.push("green");
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"playerChoices:",playerChoices)
+    }else if(playerTurn && !pushStatus){
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"playerChoices:",playerChoices)
+    }else if(!playerTurn && pushStatus){
+        compChoices.push("green");
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"compChoices:",compChoices)
+    }else if(!playerTurn && !pushStatus){
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"compChoices:",compChoices)
+    }
+}
 
-btnBlue.addEventListener("click", function(evt){
-    playerChoices.push("blue");
+function clickBlue(){
     document.getElementById("btnBlue").style.backgroundColor = "aqua";
     setTimeout(function(){
         document.getElementById("btnBlue").style.backgroundColor = "black";
-    },200);
-    console.log("playerChoices:",playerChoices);
-});
+    },300);
+    if(playerTurn && pushStatus){
+        playerChoices.push("blue");
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"playerChoices:",playerChoices)
+    }else if(playerTurn && !pushStatus){
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"playerChoices:",playerChoices)
+    }else if(!playerTurn && pushStatus){
+        compChoices.push("blue");
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"compChoices:",compChoices)
+    }else if(!playerTurn && !pushStatus){
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"compChoices:",compChoices)
+    }
+}
 
-btnYellow.addEventListener("click", function(evt){
-    playerChoices.push("yellow");
+function clickYellow(){
     document.getElementById("btnYellow").style.backgroundColor = "yellow";
     setTimeout(function(){
         document.getElementById("btnYellow").style.backgroundColor = "black";
-    },200);
-    console.log("playerChoices:",playerChoices);
-});
+    },300);
+    if(playerTurn && pushStatus){
+        playerChoices.push("yellow");
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"playerChoices:",playerChoices)
+    }else if(playerTurn && !pushStatus){
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"playerChoices:",playerChoices)
+    }else if(!playerTurn && pushStatus){
+        compChoices.push("yellow");
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"compChoices:",compChoices)
+    }else if(!playerTurn && !pushStatus){
+        console.log("playerTurn:",playerTurn,"pushStatus:",pushStatus,"compChoices:",compChoices)
+    }
+}
 
 function genRandColor(){
     let colors = ["pink", "green", "blue", "yellow"];
     let rand = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
-    console.log("Rand:",rand, colors[rand]);
+    //console.log("Rand:",rand, colors[rand]);
     return colors[rand];
 }
 
 function addCompChoice(){
-    console.log(compChoices)
-    compChoices.push(genRandColor());
-    console.log("compChoices:",compChoices)
+    let next = genRandColor();
+    pushStatus = true;
+        if(next === "pink"){
+            clickPink();
+        }else if(next === "green"){
+            clickGreen();
+        }else if(next === "blue"){
+            clickBlue();
+        }else{
+            clickYellow();
+        }
 }
 
-addCompChoice();
+function addPlayerChoice(){
+
+}
+
+function init(){
+    compTurn()
+}
+
+function changeTurn(){
+    playerTurn = !playerTurn;
+    console.log("playerTurn:",playerTurn);
+}
+
+// compChoices.forEach(function(color){
+//     console.log("Color",color)
+// })
+
+function compTurn(){
+    let delay = 1500;
+    for(let i = 0; i < compChoices.length;i++){
+        if(compChoices[i] === "pink"){
+            console.log("pushStatus:",pushStatus)
+            setTimeout(clickPink,delay)
+        }
+        else if(compChoices[i] === "green"){
+            setTimeout(clickGreen,delay)
+        }
+        else if(compChoices[i] === "blue"){
+            setTimeout(clickBlue,delay)
+        }else{
+            setTimeout(clickYellow,delay)
+        }
+        //console.log("Delay:",delay);
+        delay = delay + 750;
+    }
+    //pushStatus = true;
+    //delay = delay + 750;
+    setTimeout(addCompChoice,delay);
+}
+
+function checkAnswer(){
+    for(let i = 0; i < compChoices.length;i++){
+        if(compChoices.length !== playerChoices.length){
+            document.querySelector("#status").innerHTML = "Wrong answer, try again!";
+            break;
+        }else if(compChoices[i] === playerChoices[i]){
+            console.log("Correct match at Index:",i);
+        }
+        else{
+            console.log("Wrong match at Index:",i);
+            document.querySelector("#status").innerHTML = "Wrong answer, try again!";
+            break;
+        }
+    }
+    document.querySelector("#status").innerHTML = "Correct! Nice job! Click 'Next Level' to move on";
+}
+
+init();
